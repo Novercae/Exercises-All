284ca4bb066f083f62ffbb2a75eb6f955c6934c1
Vital Busatta Filho <98193343+Novercae@users.noreply.github.com>
2022-03-07 17:15:21 -0300

    Update palindromic-words.py

--- palindromic-words.py
+++ palindromic-words.py
@@ -1,25 +1,27 @@
-# look for words that can be rearranged and be palindromes.
-# procure por palavras que podem ser rearranjado e ser palindromas.
+"""
+look for words that can be rearranged and be palindromes.
+procure por palavras que podem ser rearranjado e ser palindromas.
+"""
 
 text = ['ciicv', 'baba', 'xyz', 'casa', 'anilina', 'bbbbaabbbbaa']
 
 
-def pali_hashtable(palavra):
-    letras = {}
-    impares = 0
-    for l in palavra:
-        if l in letras:
-            letras[l] += 1
+def pali_hashtable(word):
+    letters = {}
+    odd = 0
+    for l in word:
+        if l in letters:
+            letters[l] += 1
         else:
-            letras[l] = 1
+            letters[l] = 1
 
-    for i in letras.values():
+    for i in letters.values():
         if i%2 == 1:
-            impares += 1
-        if impares >= 2:
+            odd += 1
+        if odd >= 2:
             return False
     return True
 
 
-for palavra in text:
-    print(pali_hashtable(palavra))
+for word in text:
+    print(pali_hashtable(word))
